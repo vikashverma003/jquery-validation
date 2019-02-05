6efc99c7f37a681b304be3a26a3c2b18369a53af
vikashverma003 <vikashverma003@gmail.com>
2019-02-05 23:28:56 +0530

    jq

---
+++ jqueryajax1/submission.php
@@ -0,0 +1,8 @@
+<?php
+ 
+$emailAddress = false;
+if(isset($_POST['email'])){
+    $emailAddress = $_POST['email'];
+}
+ 
+echo 'Received email was: ' . $emailAddress;
---
+++ js/data.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['fname'], $_POST['lname']))
+{
+	
+	print_r($_POST);
+	
+}
+
+
+?>
